52510f8abb3fbadeafd3f4d0ab484347fb5c265b
lim_yungyoo <lim_yungyoo@elandsystems.com>
2016-12-26 13:22:16 +0900

    properties rollback

--- study/src/main/java/ac/moim/user/service/MembershipServiceImpl.java
+++ study/src/main/java/ac/moim/user/service/MembershipServiceImpl.java
@@ -1,9 +1,5 @@
 package ac.moim.user.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import ac.moim.user.dao.MembershipDao;
-
 public class MembershipServiceImpl implements MembershipService {
 	
 	public Boolean Enroll(String temp){
