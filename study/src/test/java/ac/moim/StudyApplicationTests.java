837582be7aa357ee78d824fe49a8d405d80e7d71
Hohoon Song <thdghgns@gmail.com>
2017-01-10 17:11:29 +0900

    add study test code

--- study/src/test/java/ac/moim/StudyApplicationTests.java
+++ study/src/test/java/ac/moim/StudyApplicationTests.java
@@ -2,7 +2,12 @@ package ac.moim;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
