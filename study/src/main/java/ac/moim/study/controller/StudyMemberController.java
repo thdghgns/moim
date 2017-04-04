8398c217232fdafd70fb223e996b7e3e5414090d
dev-adabyron <dev.adabyron@gmail.com>
2017-04-05 06:50:11 +0900

    수정

--- study/src/main/java/ac/moim/study/controller/StudyMemberController.java
+++ study/src/main/java/ac/moim/study/controller/StudyMemberController.java
@@ -1,11 +1,15 @@
 package ac.moim.study.controller;
 
 import ac.moim.study.dto.CommentDto;
+import ac.moim.study.dto.StudyDto;
 import ac.moim.study.dto.StudyMemberDto;
+import ac.moim.study.entity.Study;
 import ac.moim.study.entity.StudyMember;
 import ac.moim.study.exception.StudyBadRequestException;
 import ac.moim.study.service.CommentService;
 import ac.moim.study.service.StudyMemberService;
+import ac.moim.study.service.StudyService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,6 +31,10 @@ public class StudyMemberController {
 
 	@Autowired
 	private StudyMemberService studyMemberService;
+	
+	@Autowired
+	private StudyService studyService;
+
 
 	@Autowired
 	private CommentService commentService;
@@ -54,27 +62,36 @@ public class StudyMemberController {
 
 		return "";
 	}
-
 	@RequestMapping(value = "/enroll", method = RequestMethod.GET)
 	public String studyEnroll(Model model, HttpSession httpSession,
 			@RequestParam(value = "studyId", required = false, defaultValue = "") Integer studyId,
-			@RequestParam(value = "content", required = false, defaultValue = "") String content) {
+			@RequestParam(value = "content", required = false, defaultValue = "") String content,
+			@RequestParam(value = "userClassifier", required = false, defaultValue = "") String userClassifier) {
 
 		StudyMemberDto.Request requestStudyMember = new StudyMemberDto.Request();
 		requestStudyMember.setStudyId(studyId);
 		if (httpSession.getAttribute("userId") != null) {
 			String userId = String.valueOf(httpSession.getAttribute("userId"));
-			StudyMember studyMember = studyMemberService.saveStudyMember(studyId, userId, "teamone");
-
-			if (studyMember != null) {
-				CommentDto.Request requestComment = new CommentDto.Request();
-				if (content != null) {
-					requestComment.setContent(content);
-					requestComment.setStudyId(studyId);
-					commentService.saveComment(requestComment);
+			if(userClassifier.equals("leader")){
+				//studyMemberService.
+				
+			}else if(userClassifier.equals("teamone")){
+				studyMemberService.deleteStudyMember(studyId, userId, userClassifier);
+				
+			}else{
+				StudyMember studyMember = studyMemberService.saveStudyMember(studyId, userId, "teamone");
+				if (studyMember != null) {
+					CommentDto.Request requestComment = new CommentDto.Request();
+					if (content != null) {
+						requestComment.setContent(content);
+						requestComment.setStudyId(studyId);
+						commentService.saveComment(requestComment);
+					}
 				}
+			}	
+
+			return "views/study/detail";
 			}
-			return "views/study/detail";}
 		else{
 			return "redirect:" + "/login";
 		}
